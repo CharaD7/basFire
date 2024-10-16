b1df706496f1378cbfb9bfc82cc1b482239f9460
CharaD7 <jijakahn6@gmail.com>
2024-10-16 17:49:49 +0000

    First design of index page

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container mx-auto p-4">
+    <h1 class="text-3xl font-bold mb-4">Welcome to basFire</h1>
+    <p class="mb-4">Please sign up or login to access the dashboard</p>
+
+    <div class="space-x-4">
+      <NuxtLink class="btn btn-primary" to="/signup">Sign up</NuxtLink>
+      <NuxtLink class="btn btn-secondary" to="/login">Login</NuxtLink>
+    </div>
+  </div>
+</template>
