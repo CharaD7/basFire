a76a7e0fc735baf4bc3f91e9da62563e66a81b67
CharaD7 <jijakahn6@gmail.com>
2024-10-18 20:08:51 +0000

    Record user and user uid

--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -1,28 +1,34 @@
 import {
   getAuth,
-  createUserWithEmailAndPassword ,
+  createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
 } from "firebase/auth";
-import type { User } from "firebase/auth";
+import type { User, UserCredential } from "firebase/auth";
 
 export const useAuth = () => {
   const { $auth, $currentUser } = useNuxtApp()
 
-  const signUp = async (email: string, password: string): Promise<User> => {
+  const signUp = async (email: string, password: string): Promise<{ user: User; uid: string }> => {
     try {
-      const userCredential = await createUserWithEmailAndPassword($auth, email, password);
-      return userCredential.user;
+      const userCredential: UserCredential = await createUserWithEmailAndPassword($auth, email, password);
+      return {
+        user: userCredential.user,
+        uid: userCredential.user.uid
+      };
     } catch (error) {
       console.error("Error Signing Up: ", error);
       throw error;
     }
   };
 
-  const login = async (email: string, password: string): Promise<User> => {
+  const login = async (email: string, password: string): Promise<{ user: User; uid: string }> => {
     try {
       const userCredential = await signInWithEmailAndPassword($auth, email, password);
-      return userCredential.user;
+      return {
+        user: userCredential.user,
+        uid: userCredential.user.uid
+      };
     } catch (error) {
       console.error("Error Logging In: ", error);
       throw error;
